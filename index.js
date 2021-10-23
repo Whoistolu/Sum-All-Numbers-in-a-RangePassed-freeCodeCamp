799ab6ed6b5f0d518a1b3d881497f11f625fcabc
Whoistolu <digittolu25@gmail.com>
2021-10-23 22:28:19 +0100

    Add solution

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function sumAll(arr) {
-    return 1;
+function diffArray(arr1, arr2) {
+    let newArr = [];
+    return newArr;
   }
   
-  sumAll([1, 4]);
+  diffArray([1, 2, 3, ], [1, 2, 3, 4, 5]);
